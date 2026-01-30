8f38c1bef977d0375d971f4cbc1024ef716c0df4
Yassin Diab <yassin.diab@student.giu-uni.de>
2026-01-30 03:25:39 +0200

    feat(prisma): update schema and db config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,28 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const globalForPrisma = global as unknown as {
+  prisma?: PrismaClient;
+  pool?: pg.Pool;
+};
+
+const pool =
+  globalForPrisma.pool ??
+  new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
+  globalForPrisma.pool = pool;
 }
